d16fbcb894f7a60947acdd67a62463d83592dc4f
AlHameedha-Malik <153212607+AlHameedha-Malik@users.noreply.github.com>
2024-12-17 20:51:51 +0400

    Delete UI directory

--- UI/src/app/task/task.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component, OnInit } from '@angular/core';
-import { SharedService } from '../shared.service';
-
-@Component({
-  selector: 'app-task',
-  templateUrl: './task.component.html',
-  styleUrl: './task.component.css'
-})
-export class TaskComponent {}
